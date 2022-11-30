e4fad06c80dc1d02b075d03c27e6a77bdba113ad
panupong rong-o <tayalone@gmail.com>
2022-11-30 16:35:56 +0700

    :sparkles: add api route

--- todo/main.go
+++ todo/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/:id", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Get Todo by Id",
+		})
+	})
+
+	r.POST("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.PATCH("/:id", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "edit to do by id",
+		})
+	})
+
+	r.DELETE("/:id", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "delete to do by id",
+		})
+	})
+
+	r.DELETE("user/:id", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "delete to do by id",
+		})
+	})
+
+	r.Run(":3001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
